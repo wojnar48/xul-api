51309a559aebeec9aaa1df5a7aebbc20a735d186
Szymon Wojnar <wojnar48@gmail.com>
2018-12-03 18:36:28 -0500

    Add resolvers for  to allow for pagination on the front-end

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   filters: forwardTo('db'),
   filter: forwardTo('db'),
+  filtersConnection: forwardTo('db'),
 };
 
 module.exports = Query;
