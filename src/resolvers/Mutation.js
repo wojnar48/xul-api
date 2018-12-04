429afe6e8fb6ec2ac50116e8abd7bee939f434e5
Szymon Wojnar <wojnar48@gmail.com>
2018-12-03 19:24:09 -0500

    Update the User model with additional fields and provide mutation resolvers for the signup action

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const Mutation = {
   async createFilter(parent, args, ctx, info) {
     // TODO(SW): Only allow authenticated users to create filters
@@ -24,6 +27,39 @@ const Mutation = {
     const filter = await ctx.db.query.filter({ where }, `{ id name }`);
     // TODO(SW): Check if the user has permission to delete the filter 
     return ctx.db.mutation.deleteFilter({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    // Create a hash of the password sent in the request
+    const passwordHash = bcrypt.hashSync(args.password, 10);
+
+    // Create the user in the db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password: passwordHash,
+          // TODO(SW): Confirm why this is required 
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+
+    // Create a JWT for the user
+    const token = jwt.sign({ iserId: user.id }, process.env.APP_SECRET);
+
+    // Set the JWT token in a cookie
+    ctx.response.cookie('token', token, {
+      // makes cookie inaccessible to js
+      httpOnly: true,
+      // TODO(SW): Cnfirm optimal maxAge
+      maxAge: 1000 * 60 * 60 * 24, 
+    });
+
+    // Return the user to the browser
+    return user;
   }
 };
 
